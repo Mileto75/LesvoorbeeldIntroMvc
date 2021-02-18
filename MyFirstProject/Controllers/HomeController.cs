864c26fb5de5147bd39a2d7c3e5546e4c6607e7e
Mileto Di Marco <mileto.di.marco@howest.be>
2021-02-18 13:38:59 +0100

    Add project files.

---
+++ MyFirstProject/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult MyName()
+        {
+            return Content("<h1>Mileto<h2>","text/html");
+        }
+
+
+        public IActionResult SearchByIndex(int id)
+        {
+            return Content($"You searched product with id:{id}");
+        }
+
+        public IActionResult SearchByString(string needle)
+        {
+            return Content($"You searched product with string:{needle}");
+        }
+
+        [Route("/")]
+        [Route("Home/")]
+        [Route("Home/Index")]
+        [Route("Landing")]
+        public IActionResult Index()
+        {
+            return Content("Index","text/plain");
+        }
+
+        [Route("Home/About")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Dit is mijn eerste .NEt Core MVC app";
+
+            return View();
+        }
+
+        [Route("Home/Contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyFirstProject/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("<h2>Products</h2>","text/html");
+        }
+
+        [Route("Products/Manage/{id:int}/{todo}")]
+        public IActionResult Manage(int id,string toDo)
+        {
+            return Content($"Action:{toDo} product" +
+                $"with id: {id}");
+        }
+    }
+}
